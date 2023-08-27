7c2ab5cb0c220a5471520b7f51f7700c1a5aa198
SusanEspo <135932394+SusanEspo@users.noreply.github.com>
2023-08-27 11:18:32 -0500

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,23 @@
 
 ##Created on Sun Jul 16 15:58:32 2023
 
-##author: Susan Espinosa espo.susan@gmail.com
-
+##author: Susan Espinosa
 ##Ptyhon HW Module 3 challenge
-
-## STEP#1  - read file 
-##"C:\Users\espos\Documents\DS-BootCamp\3-Module-3-Challenge\Starter_Code\PyBank\Resources"\budget_data.csv"
-
-##"C:\Users\espos\Documents\DS-BootCamp\3-Module-3-Challenge\Starter_Code\PyBank\Resources"
-
-
 
 ##imports
 import os
 import csv
 
- 
+## STEP#1  - read file 
+
+mycvs_budget = os.path.join("/Users/espos/Documents/DS-BootCamp/3-Module-3-Challenge/Starter_Code/PyBank/Resources","budget_data.csv")
+fin_out = open("/Users/espos/Documents/DS-BootCamp/3-Module-3-Challenge/PyBank/analysis/Financial_Analysis.csv","w")
+
+
 ##Declartion
 
 ##Varibles
-mycvs_budget = os.path.join("/Users/espos/Documents/DS-BootCamp/3-Module-3-Challenge/Starter_Code/PyBank/Resources","budget_data.csv")
-ttl_mths_budget_file = 0 ## deliverable total number of months
+
 ttl_netprofit_budget_file =0
 ##ttl_mths_budget_file  holds count of months in file
 total_num_rows = 0
@@ -77,40 +73,51 @@ with open(mycvs_budget, 'r') as csvfile:   # opens file
   
   ##after the forLoop
     avg_chg =  total_profit_change / (counter -1)   ##minus 1 if for the skip of january
-
+    print_avg_chg = round(avg_chg,2)
 
     ## print required data  - fix formatting
-    print("Financial Analysis")
+    print("Financial Analysis:")
     print("------------------------------------------------------")
-    print("Total Months ", counter)
+    print("Total Months: ", counter)
     print("Total: $", ttl_netprofit_budget_file)  
-    print("Averge Change $",  avg_chg )
-    print("Greatest Increase in Profits:", current_greatest_increase_date, current_greatest_increase )
-    print("Greatest Decrease in Profits", current_greatest_decrease_date, current_greatest_decrease  )
-   
-  
-   ## ttl_netprofit_budget_file = sum(rows_in_file )
-   ## print("Buget Data has ", counter, "number of months.")
-   ## print("Buget Data has net value of ", ttl_netprofit_budget_file) ##worng total
-   ## print("Avg chang over time",  avg_chg )
-   ## print("current_greatest_increase", current_greatest_increase_date, current_greatest_increase )
-   ## print("current_greatest_decrease", current_greatest_decrease_date, current_greatest_decrease  )
-    ##i tried some """
-   
+    print("Averge Change $",  print_avg_chg )
+    print("Greatest Increase in Profits:", current_greatest_increase_date, "(",current_greatest_increase,")")
+    print("Greatest Decrease in Profits:", current_greatest_decrease_date,"(",current_greatest_decrease,")")
     
-    ##print(header_names)
-    ##print(rows_in_file)
-    ##print(total_profit_change)
-    ##print(fields_in_file)    
-     
+    
+    str_counter = str(counter)
+    str_ttl_netprofit_budget_file = str(ttl_netprofit_budget_file)
+    str_avg_chg = str(print_avg_chg)
+    str_current_greatest_increase_date = str(current_greatest_increase_date)
+    str_current_greatest_increase = str(current_greatest_increase)
+    str_current_greatest_decrease_date = str(current_greatest_decrease_date)
+    str_current_greatest_decrease = str(current_greatest_decrease)
+    
+    ## print required data  - fix formatting
+    fin_out.write("Financial Analysis:")
+    fin_out.write("\n-------------------------------------------\n")
+    fin_out.write("Total Months: ")
+    fin_out.write(str_counter)
+    fin_out.write("\nTotal: $")
+    fin_out.write(str_ttl_netprofit_budget_file)  
+    fin_out.write("\nAverge Change $")
+    fin_out.write(str_avg_chg )
+    
+    fin_out.write("\nGreatest Increase in Profits:  " )
+    fin_out.write(str_current_greatest_increase_date)
+    fin_out.write(" " )
+    fin_out.write(str_current_greatest_increase)
+    
+    #fin_out.write("Greatest Decrease in Profits:", current_greatest_decrease_date,"(",current_greatest_decrease,")")
+   
+    fin_out.write("\nGreatest Increase in Profits:  " )
+    fin_out.write(str_current_greatest_decrease_date)
+    fin_out.write(" " )
+    fin_out.write(str_current_greatest_decrease)
+ 
+    fin_out.write("\n--------------------------------------------\n")
 
-# for row in  csv_read_it:
-    
-#     if (row[1]) != " ":
-#         ttl_mths_budget_file = ttl_mths_budget_file + (row[2])
-#         print(row)
-         
- 
- 
-     
+
+    fin_out.close()
+
      
